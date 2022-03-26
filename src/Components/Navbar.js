3f6e32fbd31807184a0af1d87663b6c5fa9dbc84
Rupesh Gaikwad <rupeshgaikwad3333@gmail.com>
2022-03-27 01:05:05 +0530

    Hangerber menu implemented

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,23 +1,50 @@
-import React from 'react'
+import React, { useRef, useState, useEffect } from 'react'
 import './css/nav.css';
 import logo from './images/VentureLatticeLogo.png';
 import user from './images/current_user.png'
 export default function Navbar() {
+
+  const [menuOn, setMenuOn] = useState(false);
+  let hangerbarMenuRef = useRef();
+  const toggleMenu = () =>{
+    setMenuOn(!menuOn);
+  }
+
+  useEffect( () => {
+    let handler = (event) => {
+      if( menuOn && !hangerbarMenuRef.current.contains(event.target)){
+        setMenuOn(!menuOn);
+      }
+    };
+    document.addEventListener("mousedown", handler);
+
+    return () => {
+      document.removeEventListener("mousedown", handler);
+    };
+});
+
   return (
     <nav>
         <div className="nav-content">
             <img className="company_logo" src={logo} alt="company logo"/>
-            <ul>
-                <li>Home</li>
-                <li>Services</li>
-                <li>Contact</li>
-            </ul>
+            <div ref={hangerbarMenuRef} className={`nav_links ${menuOn ? "nav_links_active" : ""}`}>
+              <div className="menu_user">
+                  <img id="user_profile" src={user} alt="user_profile"/>
+                  <p>Megan Fox</p>
+              </div>
+              <span id="close_menu" onClick={toggleMenu}>&#10005;</span>
+              <ul>
+                  <li>Home</li>
+                  <li>Services</li>
+                  <li>Contact</li>
+              </ul>
+            </div>
             <div className="user">
                 <img id="user_profile" src={user} alt="user_profile"/>
                 <p>Megan Fox</p>
             </div>
 
-            <div className="hamburger_menu">
+            <div className="hamburger_menu" onClick={toggleMenu}>
               <div className="line"></div>
               <div className="line"></div>
               <div className="line"></div>
